a32b3e3f0feaea36dcae48a20fecc017280999e3
Sergio Ricardo <sergio.rpq@gmail.com>
2021-08-28 14:20:37 -0300

    Entradas java 001

---
+++ java/basico01/entradas/Main.java
@@ -0,0 +1,15 @@
+/*
+	Primeiro programa Java para entrada de dados
+*/
+
+
+public class Main{
+
+	public static void main(String[] args){
+
+		System.out.println("== Entrada de dados Java 001 ==");
+
+	}
+
+
+}
